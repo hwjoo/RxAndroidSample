9d5202f4c0b057903e5ede1e9a7ad913b7cb1fd8
hwjoo <savior0826@gmail.com>
2017-02-07 18:02:57 +0900

    add

--- app/src/main/java/com/hwjoo/rxandroidsample/MainActivity.java
+++ app/src/main/java/com/hwjoo/rxandroidsample/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +17,11 @@ import java.util.Random;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import rx.Observable;
+import rx.Observer;
+import rx.Scheduler;
 import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.functions.Func1;
@@ -24,9 +29,11 @@ import rx.functions.Func1;
 import static android.R.attr.right;
 import static android.R.attr.text;
 import static android.R.attr.value;
+import static android.R.id.empty;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.editText)
+    EditText editText;
     @BindView(R.id.textView)
     TextView textView;
     @BindView(R.id.btn)
@@ -39,58 +46,88 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        A a = new A();
+        a.setName("Joo, hyunwoo");
+        a.setAge(29);
+        Observable<A> simpleObservable = Observable.just(a);
+//        Observable<String> simpleObservable = Observable.just("a");
+        Observer<A> observer = new Observer<A>() {
+            @Override
+            public void onCompleted() {
 
-//        Observable<String> simpleObservable = Observable.just("Hello Lambda!!");
-//        simpleObservable.map(text->text.length())
-//                .subscribe(length->textView.setText("length: "+length));
-//
-//        RxView.clicks(btn).map(event->new Random().nextInt())
-//                .subscribe(value->{
-//                    textView.setText("number:" + value.toString());
-//                },throwable -> {
-//                    Log.d("TEST","Error : "+ throwable.getMessage());
-//                    throwable.printStackTrace();
-//                });
-        Observable<String> lefts = RxView.clicks(btn)
-                .map(event->"left");
-        Observable<String> rights = RxView.clicks(btn1)
-                .map(event->"right");
-        Observable<String> together = Observable.merge(lefts, rights);
-        together.subscribe(text->textView.setText(text));
-        together.map(text-> text.toUpperCase())
-                .subscribe(text-> Toast.makeText(this, text, Toast.LENGTH_SHORT).show());
+            }
 
-//        Observable<String> simpleObservable =
-//                Observable.create(new Observable.OnSubscribe<String>() {
-//                    @Override
-//                    public void call(Subscriber<? super String> subscriber) {
-//                        subscriber.onNext("Hellow RxAndroid !!");
-//                        subscriber.onCompleted();
-//                    }
-//                });
-//        simpleObservable.map(new Func1<String, Integer>() {
-//            @Override
-//            public Integer call(String s) {
-//                return s.length();
-//            }
-//        }).subscribe(new Action1<Integer>() {
-//            @Override
-//            public void call(Integer s) {
-//                textView.setText("length : " + s);
-//            }
-//        }, new Action1<Throwable>() {
-//            @Override
-//            public void call(Throwable throwable) {
-//                Log.d("TEST",throwable.toString());
-//            }
-//        }, new Action0() {
-//            @Override
-//            public void call() {
-//
-//            }
-//        });
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(A a) {
+
+            }
+        };
+        Subscriber<A> subscriber = new Subscriber<A>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(A a) {
+
+            }
+        };
+        Subscription subscription = new Subscription() {
+            @Override
+            public void unsubscribe() {
+
+            }
+
+            @Override
+            public boolean isUnsubscribed() {
+                return false;
+            }
+        };
+
+        simpleObservable.observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        o-> {
+                            textView.setText("name :" + o.getName() + "\n" + "age :" + o.getAge());
+                        },
+                        e->textView.setText(e.getMessage())
+
+                );
+
+
 
 
 
     }
+    class A {
+        String name;
+        int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+
 }
